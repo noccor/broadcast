7a507324a4d86920d40c1aef287d1c68448d6c89
Connor James <connor@MBP-ConnorJ-292149.local>
2020-06-02 13:00:17 +0200

    chore(routes); add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount OpenApi::Rswag::Ui::Engine => '/api-docs'
   mount OpenApi::Rswag::Api::Engine => '/api-docs'
+
+  resources :broadcasts, only: [:index, :create, :show, :destroy]
+  resources :subscriptions, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
