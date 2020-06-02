d307a0b9886695ee444e31e3a682b14c3472298b
Connor James <connor@MBP-ConnorJ-292149.local>
2020-06-02 23:36:40 +0200

    fix(subcription): model class naming issue, a subscription shall never a broadcast be

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,8 +1,9 @@
-class Broadcast < ActiveRecord::Base
+class Subscription < ActiveRecord::Base
   belongs_to :broadcast
-  
+
   validates :broadcast_id, presence: true
   validates :email, presence: true
   validates :firstname, presence: true
   validates :surname, presence: true
 end
+on
